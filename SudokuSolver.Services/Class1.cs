ef5464f15d60737c7c0ceb3a12d638a368bb3159
Bryan Garcia <bryan@segfaulted.com>
2019-05-29 11:54:14 -0500

    Initial Commit

---
+++ SudokuSolver.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SudokuSolver.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ SudokuSolver/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SudokuSolver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
